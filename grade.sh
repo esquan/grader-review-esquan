6cdab8b5c38fd056d7408cc34f5699c4456b437f
Evelyn Quan <esquan@ucsd.edu>
2023-05-25 13:39:43 -0700

    updated grade.sh

--- grade.sh
+++ grade.sh
@@ -17,29 +17,37 @@ echo 'Finished cloning'
 
 if [[ -f ./student-submission/ListExamples.java ]]
 then 
-    cp student-submission/ListExamples.java TestListExamples.java ./lib/* grading-area
+    cp student-submission/ListExamples.java TestListExamples.java grading-area
+    cp -r ./lib/ grading-area
 else
     echo "Wrong file submitted."
 fi
 
-javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" grading-area/*.java 2> /dev/null
+cd grading-area
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
 
 if [[ $? != 0 ]]
 then
     echo "File did not compile"
     exit 1
 else
-    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore grading-area/TestListExamples.java > outcome.txt
+    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > outcome.txt
 fi
 
 result=`grep "FAILURES!!!" outcome.txt`
 expected="FAILURES!!!"
 
+failed=`grep "Tests run:" outcome.txt`
+COUNT_FAIL=${failed:25:1}
+COUNT_TOTAL=${failed:11:1}
+COUNT_GRADE=$(($COUNT_TOTAL-$COUNT_FAIL))
+
 if [[ $result == $expected ]]
 then   
-    echo Your tests give the following Junit error:
-    cat outcome.txt
+    echo $COUNT_FAIL "test(s) failed"
+    echo Grade: $COUNT_GRADE/$COUNT_TOTAL
 else
     echo All tests passed!
+    echo Grade: 1/1
 fi
 
